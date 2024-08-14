f200d8a01acbfca4962e4aaa1e6dc435461c39b1
myscuddy <myscuddy@gmail.com>
2024-08-13 23:01:36 -0400

    Improvements to code to handle HEIF extensions, multiple files, better error handling & logging, improved documentation

--- convert-all-files.py
+++ convert-all-files.py
@@ -1,8 +1,10 @@
+from ctypes.wintypes import BOOLEAN
 from functools import wraps
 import glob, sys, os
 import argparse
 import logging
 import traceback
+from xmlrpc.client import boolean
 
 import myfuncs as funcs
 import my_constants as const
@@ -10,33 +12,48 @@ import my_constants as const
 from PIL import Image, UnidentifiedImageError
 from pillow_heif import HeifImagePlugin
 
-
-
 def main():
-    # -- Create custom logger
-    logger = funcs.setup_logger()
-
-    funcs.log_info_message (f"=============== Inside [{funcs.get_method_name()}] ===============")
-
-    parser=argparse.ArgumentParser()
+    parser=argparse.ArgumentParser(
+        exit_on_error=False,
+        description='''This script allows for conveting one or more image files in a folder into another format. The source & target 
+        folders are passed as arguments into the script.''',
+        epilog='''Note: If "follow_recursively" is set to 1 a source folder is passed with or without source extension, remember that this script will perform its 
+        conversion in a resursive manner and go through the files in source folder as well those in child folders.'''
+    )
 
     # -- Parse arguments
-    parser.add_argument('-source_path_with_img_files', required=True, help='Full path to source image file(s)')
-    parser.add_argument('-src_fmt', required=False, help='Format to filter the source image file(s) with')
-    parser.add_argument('-target_path_for_converted_files', required=False, help='Full path to where converted image file(s) will be written to')
-    parser.add_argument('-target_ext', required=True, help='Format to convert the source image file(s) to')
-    parser.add_argument('-help', required=False, help='Help about the script')
+    parser.add_argument('-source_path_with_img_files', type=str, required=True, help='Full path to source image file(s)')
+    parser.add_argument('-src_ext', required=False, type=str, help='Format to filter the source image file(s) with')
+    parser.add_argument('-target_path_for_converted_files', type=str, required=False, help='Full path to where converted image file(s) will be written to')
+    parser.add_argument('-target_ext', type=str, required=True, help='Format to convert the source image file(s) to')
+    parser.add_argument('-follow_recursively', type=int, required=False, default=0, help='Whether script should recursively look for files with "src_ext" under the "source_path_with_img_files" path. Default is False.')
+    parser.add_argument('-overide_write', type=int, required=False, default=0, help='Whether to overide & write target file if it exists')
+    parser.add_argument('-overide_debug', type=int, required=False, default=0, help='Whether to enable/override debug on and set log levels to INFO (most verbose level)')
+    # parser.add_argument('-help', required=False, help='Help about the script')
+
+    from rich.traceback import install
+    install(show_locals=True)
 
     try :
         args = parser.parse_args()
 
+        # -- Create custom logger
+        logger = funcs.setup_logger()
+        if args.overide_debug == 1:
+            const.ISDEBUG = args.overide_debug
+            logger.setLevel(logging.DEBUG) 
+
+        funcs.log_info_message (f"=============== Inside [{funcs.get_method_name()}] ===============")
+
         processed_file_count=funcs.process_multiple_image_files(args)
         funcs.log_info_message (f"Finished conversion of [{processed_file_count}] files from [{args.source_path_with_img_files}] ...")
 
-    except SystemExit as se:
-        funcs.log_error_message (f"ERROR SystemExit exception encountered:: *** Error: [{str(se)}] ***")
+    except argparse.ArgumentError as ae:
+        funcs.log_error_message (f"ERROR ArgumentError exception encountered:: *** Error: [{str(ae)}] ***")
     except Exception as e:
         funcs.log_error_message (f"ERROR converting files from {args.source_path_with_img_files} due to *** Error: [{str(e)}] ***")
+    except SystemExit as se:
+        funcs.log_error_message (f"ERROR SystemExit exception encountered:: *** Error: [{str(se)}] ***")
     finally:
         funcs.log_info_message (f"=============== Exiting [{funcs.get_method_name()}] ===============")
 
@@ -44,12 +61,3 @@ def main():
 if __name__ == "__main__":
     sys.exit(main())
 
-
-
-
-# --- OLD CODE
-# from PIL import Image
-# import glob
-# for file in glob.glob("images/*.jpg"):
-#     image = Image.open(file)
-#     image.save(file.replace("jpg", "png"))
---
+++ myimageconverter.py
@@ -0,0 +1,73 @@
+from fileinput import filename
+from PIL import Image, UnidentifiedImageError
+from pillow_heif import HeifImagePlugin
+
+import myfuncs as funcs
+import my_constants as const
+
+
+"""
+Save file and return the converted file with complete path as passed to it
+"""
+# @log_decorator
+def save_img_file (rgb_im, cnv_img_file_with_path, quality_in=-666) -> str:
+    try:
+        funcs.log_debug_message (f"{funcs.get_method_name()}::cnv_img_file_with_path [{cnv_img_file_with_path}], quality% [{quality_in}] ...")
+
+        if quality_in == -666:
+            rgb_im.save(fp=cnv_img_file_with_path)
+        else:
+            rgb_im.save(fp=cnv_img_file_with_path, quality=quality_in)
+
+        funcs.log_debug_message (f"{funcs.get_method_name()}::Successfully converted file to [{cnv_img_file_with_path}] ...")
+        return cnv_img_file_with_path
+    except Exception as e:
+        # raise Exception(f"{funcs.get_method_name()}::Error occurred [{e}]")
+        raise e
+
+
+"""
+Internal class to handle
+"""
+class MyImageConverter:
+    def convert_to_nonjpeg(self, img_file_with_path, in_ext, cnv_img_file_with_path):
+        funcs.log_debug_message (f"{funcs.get_method_name()}::img_file_with_path [{img_file_with_path}], cnv_img_file_with_path [{cnv_img_file_with_path}], quality% [{None}] ...")
+        rgb_im = Image.open(img_file_with_path)
+        save_img_file(rgb_im, cnv_img_file_with_path, const.DEFAULT_JPEG_QUALITY_IN_PERCENT)
+
+    def convert_to_jpeg(self, img_file_with_path, in_ext, cnv_img_file_with_path):
+        funcs.log_debug_message (f"{funcs.get_method_name()}::img_file_with_path [{img_file_with_path}], cnv_img_file_with_path [{cnv_img_file_with_path}], quality% [{const.JPEG_QUALITY_IN_PERCENT}] ...")
+        """
+        Need to perform one extra step when converting from .png to .jpg
+        """
+        rgb_im = Image.open(img_file_with_path).convert('RGB')
+        save_img_file(rgb_im, cnv_img_file_with_path, const.JPEG_QUALITY_IN_PERCENT)
+
+    # def convert_heic_to_png(self, img_file_with_path, in_ext, cnv_img_file_with_path, target_ext):
+    #     funcs.log_debug_message (f"{funcs.get_method_name()}::img_file_with_path [{img_file_with_path}], cnv_img_file_with_path [{cnv_img_file_with_path}], target_ext [{target_ext}], quality% [{const.JPEG_QUALITY_IN_PERCENT}] ...")
+    #     import pillow_heif
+    #     pillow_heif.register_heif_opener()
+
+    #     if img_file_with_path.endswith(('.heic', '.heif', '.HEIC', '.HEIF')):
+    #         img = Image.open(img_file_with_path)
+    #         img.format(target_ext.replace('.','')) # type: ignore
+    #         # img.save('c:\image_name.png', format('png'))
+    #         save_img_file(img, img_file_with_path, in_ext, cnv_img_file_with_path, const.JPEG_QUALITY_IN_PERCENT if target_ext in ["jpg","jpeg"] else const.DEFAULT_JPEG_QUALITY_IN_PERCENT)
+
+    def convert_heic_to_format(self, img_file_with_path, in_ext, cnv_img_file_with_path, target_ext):
+        funcs.log_debug_message (f"{funcs.get_method_name()}::img_file_with_path [{img_file_with_path}], cnv_img_file_with_path [{cnv_img_file_with_path}], target_ext [{target_ext}], quality% [{const.JPEG_QUALITY_IN_PERCENT}] ...")
+        from wand.image import Image
+
+        supported_extensions=('.heic', '.heif', '.HEIC', '.HEIF')
+        if img_file_with_path.endswith(supported_extensions):
+            """Convert HEIC file to JPG."""
+            with Image(filename=img_file_with_path) as img:
+                img.format = target_ext.replace('.','')
+                """Convert image's """
+                if target_ext in ('jpg', 'jpeg'):
+                    img.convert('RGB')
+                    img.compression_quality = const.JPEG_QUALITY_IN_PERCENT
+                    img.save(filename=cnv_img_file_with_path)
+        else:
+            raise funcs.CustomError(f"Source file extension is not in {supported_extensions} - {funcs.get_method_name()}::img_file_with_path [{img_file_with_path}], cnv_img_file_with_path [{cnv_img_file_with_path}], target_ext [{target_ext}], quality% [{const.JPEG_QUALITY_IN_PERCENT}] ...")
+
